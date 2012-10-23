3a3e301afbd257d11f770f7e3256ea6ff78bec40
Dan Buch <d.buch@modcloth.com>
2012-10-22 21:20:53 -0400

    Collapsing some nesting
    with colons!

--- lib/git/duet/author_mapper.rb
+++ lib/git/duet/author_mapper.rb
@@ -1,59 +1,56 @@
 require 'yaml'
+require 'git/duet'
 
-module Git
-  module Duet
-    class AuthorMapper
-      attr_accessor :authors_file
+class Git::Duet::AuthorMapper
+  attr_accessor :authors_file
 
-      def initialize(authors_file = nil)
-        @authors_file = authors_file ||
-                        ENV['GIT_DUET_AUTHORS_FILE'] ||
-                        File.join(ENV['HOME'], '.git-authors')
-      end
+  def initialize(authors_file = nil)
+    @authors_file = authors_file ||
+                    ENV['GIT_DUET_AUTHORS_FILE'] ||
+                    File.join(ENV['HOME'], '.git-authors')
+  end
 
-      def map(*initials_list)
-        author_map = {}
-        initials_list.each do |initials|
-          author_map[initials] = author_info(initials)
-        end
-        author_map
-      end
-
-      private
-      def author_info(initials)
-        author, username = author_map.fetch(initials).split(/;/).map(&:strip)
-
-        if email_addresses[initials]
-          author_email = email_addresses[initials]
-        elsif username
-          author_email = "#{username}@#{email_domain}"
-        else
-          author_name_parts = author.split
-          author_email = "#{author_name_parts.first[0,1].downcase}." <<
-                         "#{author_name_parts.last.downcase}@#{email_domain}"
-        end
-
-        {
-          name: author,
-          email: author_email
-        }
-      end
-
-      def author_map
-        @author_map ||= (cfg['authors'] || cfg['pairs'])
-      end
-
-      def email_addresses
-        @email_addresses ||= (cfg['email_addresses'] || {})
-      end
-
-      def email_domain
-        @email_domain ||= cfg.fetch('email').fetch('domain')
-      end
-
-      def cfg
-        @cfg ||= YAML.load(IO.read(@authors_file))
-      end
+  def map(*initials_list)
+    author_map = {}
+    initials_list.each do |initials|
+      author_map[initials] = author_info(initials)
     end
+    author_map
+  end
+
+  private
+  def author_info(initials)
+    author, username = author_map.fetch(initials).split(/;/).map(&:strip)
+
+    if email_addresses[initials]
+      author_email = email_addresses[initials]
+    elsif username
+      author_email = "#{username}@#{email_domain}"
+    else
+      author_name_parts = author.split
+      author_email = "#{author_name_parts.first[0,1].downcase}." <<
+      "#{author_name_parts.last.downcase}@#{email_domain}"
+    end
+
+    {
+      name: author,
+      email: author_email
+    }
+  end
+
+  def author_map
+    @author_map ||= (cfg['authors'] || cfg['pairs'])
+  end
+
+  def email_addresses
+    @email_addresses ||= (cfg['email_addresses'] || {})
+  end
+
+  def email_domain
+    @email_domain ||= cfg.fetch('email').fetch('domain')
+  end
+
+  def cfg
+    @cfg ||= YAML.load(IO.read(@authors_file))
   end
 end
--- lib/git/duet/command_methods.rb
+++ lib/git/duet/command_methods.rb
@@ -1,46 +1,42 @@
 require 'git/duet'
 
-module Git
-  module Duet
-    module CommandMethods
-      private
-      def report_env_vars
-        var_map.each do |key,value|
-          STDOUT.puts "#{key}='#{value}'"
-        end
-      end
+module Git::Duet::CommandMethods
+  private
+  def report_env_vars
+    var_map.each do |key,value|
+      STDOUT.puts "#{key}='#{value}'"
+    end
+  end
 
-      def write_env_vars
-        out = []
-        var_map.each do |key,value|
-          out << "#{key}='#{value}'"
-        end
-        in_repo_root do
-          File.open(env_cache_path, 'w') do |f|
-            f.puts out.join($/)
-          end
-        end
-      end
-
-      def env_cache_path
-        '.git/hooks/git-duet-env-cache.txt'
-      end
-
-      def in_repo_root
-        Dir.chdir(`git rev-parse --show-toplevel`.chomp) do
-          yield
-        end
-      end
-
-      def exec_check(command)
-        output = `#{command}`
-        if $?.exitstatus != 0
-          raise StandardError.new(
-            "Command #{command.inspect} exited with #{$?.to_i}"
-          )
-        end
-        output
+  def write_env_vars
+    out = []
+    var_map.each do |key,value|
+      out << "#{key}='#{value}'"
+    end
+    in_repo_root do
+      File.open(env_cache_path, 'w') do |f|
+        f.puts out.join($/)
       end
     end
+  end
+
+  def env_cache_path
+    '.git/hooks/git-duet-env-cache.txt'
+  end
+
+  def in_repo_root
+    Dir.chdir(`git rev-parse --show-toplevel`.chomp) do
+      yield
+    end
+  end
+
+  def exec_check(command)
+    output = `#{command}`
+    if $?.exitstatus != 0
+      raise StandardError.new(
+        "Command #{command.inspect} exited with #{$?.to_i}"
+      )
+    end
+    output
   end
 end
