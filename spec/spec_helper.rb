e821bf0223934ceeeb5945fb6469ebce9681e327
Dan Buch <dan@meatballhat.com>
2012-10-27 21:02:59 -0400

    Only switching on the music for darwin
    since the mpeg123 text output on linux is just ridicurous.  Windows
    probably doesn't work anyway, but I don't have a good way to find out.
    Whatever.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 require 'rubygems'
 require 'bundler/setup'
 
+require 'rbconfig'
 require 'simplecov'
 require 'pry'
+
+RSpec.configure do |c|
+  # No music allowed for neckbeards or polo shirts.
+  if RbConfig::CONFIG['host_os'] =~ /darwin/i
+    c.formatter = 'NyanCatMusicFormatter'
+  else
+    c.formatter = 'NyanCatFormatter'
+  end
+end
